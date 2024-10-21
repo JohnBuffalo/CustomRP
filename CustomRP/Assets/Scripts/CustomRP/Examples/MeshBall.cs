a86ba7b146d1840d4bd69e6af09737cd3344e86a
junbai <jun_bai@droidhang.com>
2024-10-21 17:24:36 +0800

    [add] MeshBall test with lightProbe

--- CustomRP/Assets/Scripts/CustomRP/Examples/MeshBall.cs
+++ CustomRP/Assets/Scripts/CustomRP/Examples/MeshBall.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Random = UnityEngine.Random;
 
 namespace HopsInAMaltDream.Examples
@@ -13,7 +14,6 @@ namespace HopsInAMaltDream.Examples
         private static int smoothnessId = Shader.PropertyToID("_Smoothness");
         [SerializeField] private Mesh mesh = default;
         [SerializeField] private Material material = default;
-
         [SerializeField] private int instanceCount = 100;
 
         Matrix4x4[] matrices;
@@ -25,6 +25,8 @@ namespace HopsInAMaltDream.Examples
 
         private float percent;
         [SerializeField] private float speed = 1;
+        [SerializeField] private bool move = false;
+        [SerializeField] private LightProbeProxyVolume lightProbeVolume  = null;
         private void Awake()
         {
             matrices = new Matrix4x4[instanceCount];
@@ -34,10 +36,10 @@ namespace HopsInAMaltDream.Examples
             smoothness = new float[instanceCount];
             for (int i = 0; i < matrices.Length; i++)
             {
-                matrices[i] = Matrix4x4.TRS(Random.insideUnitSphere * 10f, 
+                matrices[i] = Matrix4x4.TRS(Random.insideUnitSphere * 10f,
                 Quaternion.Euler(Random.value * 360f, Random.value * 360f, Random.value * 360f),
-                 Vector3.one*Random.Range(0.5f,1.5f));
-                baseColors[i] = new Vector4(Random.value, Random.value, Random.value, Random.Range(0.2f,1f));
+                 Vector3.one * Random.Range(0.5f, 1.5f));
+                baseColors[i] = new Vector4(Random.value, Random.value, Random.value, Random.Range(0.2f, 1f));
                 metallic[i] = Random.value < 0.25f ? 1f : 0f;
                 smoothness[i] = Random.Range(0.025f, 0.975f);
             }
@@ -45,6 +47,48 @@ namespace HopsInAMaltDream.Examples
 
         private void Update()
         {
+            Move();
+            if (block == null)
+            {
+                block = new MaterialPropertyBlock();
+            }
+            block.SetVectorArray(baseColorId, baseColors);
+            block.SetFloatArray(metallicId, metallic);
+            block.SetFloatArray(smoothnessId, smoothness);
+
+            var positions = new Vector3[instanceCount];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = matrices[i].GetColumn(3);
+            }
+            var lightProbes = new SphericalHarmonicsL2[instanceCount];
+            LightProbes.CalculateInterpolatedLightAndOcclusionProbes(
+                positions, lightProbes, null
+            );
+
+            var lightProbeUsage = lightProbeVolume?LightProbeUsage.UseProxyVolume:LightProbeUsage.CustomProvided;
+            LightProbes.CalculateInterpolatedLightAndOcclusionProbes(positions, lightProbes, null);
+            Graphics.DrawMeshInstanced(mesh, 0, material, GetMatries(), instanceCount, block,
+            ShadowCastingMode.On, true, 0, null, lightProbeUsage, lightProbeVolume);
+
+            block.CopySHCoefficientArraysFrom(lightProbes);
+        }
+
+
+        Matrix4x4[] GetMatries(){
+            if(!move){
+                return matrices;
+            }else{
+                return curMatrices;
+            }
+        }
+
+        void Move()
+        {
+            if (!move)
+            {
+                return;
+            }
 
             if (percent >= 100 || percent < 0)
             {
@@ -58,14 +102,6 @@ namespace HopsInAMaltDream.Examples
                 curMatrices[i] = Matrix4x4.TRS(pos * 10f, matrices[i].rotation, matrices[i].lossyScale);
             }
 
-            if (block == null)
-            {
-                block = new MaterialPropertyBlock();
-            }
-            block.SetVectorArray(baseColorId, baseColors);
-            block.SetFloatArray(metallicId, metallic);
-            block.SetFloatArray(smoothnessId, smoothness);
-            Graphics.DrawMeshInstanced(mesh, 0, material, curMatrices, instanceCount, block);
         }
     }
 }
