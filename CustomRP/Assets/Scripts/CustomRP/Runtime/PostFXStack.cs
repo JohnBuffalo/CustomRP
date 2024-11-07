dbdf34e6e9d044352e0adaed26333a6d0e152abf
junbai <jun_bai@droidhang.com>
2024-11-07 17:38:23 +0800

    [add] bloom

--- CustomRP/Assets/Scripts/CustomRP/Runtime/PostFXStack.cs
+++ CustomRP/Assets/Scripts/CustomRP/Runtime/PostFXStack.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 namespace MaltsHopDream
 {
@@ -7,10 +8,16 @@ namespace MaltsHopDream
     {
         enum Pass
         {
+            BloomHorizontal,
+            BloomVertical,
+            BloomCombine,
             Copy
         }
         private const string buffName = "Post FX";
-        int fxSourceId = Shader.PropertyToID("_PostFXSource");
+
+        private int 
+            fxSourceId = Shader.PropertyToID("_PostFXSource"),
+            fxSource2Id = Shader.PropertyToID("_PostFXSource2");
         
         
         private CommandBuffer buffer = new CommandBuffer()
@@ -27,24 +34,19 @@ namespace MaltsHopDream
         public bool IsActive => settings != null;
 
         #region Bloom
-        private const int maxBloomPyramidLevels = 16;
         private int bloomPyramidId;
         #endregion
-
-        public PostFXStack()
-        {
-            bloomPyramidId = Shader.PropertyToID("_BloomPyramid0");
-            for (int i = 1; i < maxBloomPyramidLevels; i++)
-            {
-                Shader.PropertyToID("_BloomPyramid" + i);//PropertyID 是递增的, 所以一次性申明. 后续可通过 bloomPyramidId + offset 获取
-            }
-        }
 
         public void Setup(ScriptableRenderContext context, Camera camera, PostFXSettings settings)
         {
             this.context = context;
             this.camera = camera;
             this.settings = camera.cameraType <= CameraType.SceneView ? settings : null;
+            bloomPyramidId = Shader.PropertyToID("_BloomPyramid0");
+            for (int i = 1; i < settings.Bloom.maxIterations * 2; i++)
+            {
+                Shader.PropertyToID("_BloomPyramid" + i);//PropertyID 是递增的, 所以一次性申明. 后续可通过 bloomPyramidId + offset 获取
+            }
             ApplySceneViewState();
         }
 
@@ -69,8 +71,14 @@ namespace MaltsHopDream
             buffer.BeginSample("Bloom");
             PostFXSettings.BloomSettings bloom = settings.Bloom;
             int width = camera.pixelWidth / 2, height = camera.pixelHeight / 2;
+            if (bloom.maxIterations == 0 || height < bloom.downscaleLimit || width < bloom.downscaleLimit)
+            {
+                Draw(sourceId, BuiltinRenderTextureType.CameraTarget, Pass.Copy);
+                buffer.EndSample("Bloom");
+                return;
+            }
             RenderTextureFormat format = RenderTextureFormat.Default;
-            int fromId = sourceId, toId = bloomPyramidId;
+            int fromId = sourceId, toId = bloomPyramidId + 1;
             int i;
             for (i = 0; i < bloom.maxIterations; i++)
             {
@@ -78,18 +86,39 @@ namespace MaltsHopDream
                 {
                     break;
                 }
+                int midId = toId - 1;
+                buffer.GetTemporaryRT(midId, width, height, 0 ,FilterMode.Bilinear, format);
                 buffer.GetTemporaryRT(toId, width, height, 0, FilterMode.Bilinear, format);
-                Draw(fromId, toId,Pass.Copy);
+                Draw(fromId, midId, Pass.BloomHorizontal);
+                Draw(midId, toId, Pass.BloomVertical);
                 fromId = toId;
-                toId++;
+                toId +=2;
                 width /= 2;
                 height /= 2;
             }
-            Draw(fromId, BuiltinRenderTextureType.CameraTarget,Pass.Copy);
-            for (i -= 1; i >= 0; i--)
+
+            if (i > 1)
             {
-                buffer.ReleaseTemporaryRT(bloomPyramidId + i);
+                buffer.ReleaseTemporaryRT(fromId - 1);
+                toId -= 5;
+                for (i -= 1; i > 0; i--)
+                {
+                    buffer.SetGlobalTexture(fxSource2Id, toId + 1);
+                    Draw(fromId, toId,Pass.BloomCombine);
+                    buffer.ReleaseTemporaryRT(fromId);
+                    buffer.ReleaseTemporaryRT(toId+1);
+                    fromId = toId;
+                    toId -= 2;
+                }
             }
+            else
+            {
+                buffer.ReleaseTemporaryRT(bloomPyramidId);
+            }
+
+            buffer.SetGlobalTexture(fxSource2Id, sourceId);
+            Draw(fromId, BuiltinRenderTextureType.CameraTarget, Pass.BloomCombine);
+            buffer.ReleaseTemporaryRT(fromId);
             buffer.EndSample("Bloom");
         }
     }
--- CustomRP/Assets/Scripts/CustomRP/Shaders/PostFXStack.shader
+++ CustomRP/Assets/Scripts/CustomRP/Shaders/PostFXStack.shader
@@ -13,6 +13,36 @@
 
         Pass
         {
+            Name "Bloom Horizontal"
+            HLSLPROGRAM
+            #pragma target 3.5
+            #pragma vertex DefaultPassVertex
+            #pragma fragment BloomHorizontalPassFragment
+            ENDHLSL
+        }
+
+        Pass
+        {
+            Name "Bloom Vertical"
+            HLSLPROGRAM
+            #pragma target 3.5
+            #pragma vertex DefaultPassVertex
+            #pragma fragment BloomVerticalPassFragment
+            ENDHLSL
+        }
+
+        Pass
+        {
+            Name "Bloom Combine"
+            HLSLPROGRAM
+            #pragma target 3.5
+            #pragma vertex DefaultPassVertex
+            #pragma fragment BloomCombinePassFragment
+            ENDHLSL
+        }
+
+        Pass
+        {
             Name "Copy"
             HLSLPROGRAM
             #pragma target 3.5
