03de1953b1927866ba7bd5b027eb020dce0af20a
junbai <jun_bai@droidhang.com>
2024-11-07 11:25:15 +0800

    [add] uv coordinations flip

--- CustomRP/Assets/Scripts/CustomRP/Shaders/PostFXStackPass.hlsl
+++ CustomRP/Assets/Scripts/CustomRP/Shaders/PostFXStackPass.hlsl
@@ -1,6 +1,9 @@
 ﻿#ifndef CUSTOM_POST_FX_PASSES_INCLUDED
 #define CUSTOM_POST_FX_PASSES_INCLUDED
 
+TEXTURE2D(_PostFXSource);
+SAMPLER(sampler_linear_clamp);
+
 struct Varyings {
     float4 positionCS : SV_POSITION;
     float2 screenUV : VAR_SCREEN_UV;
@@ -17,11 +20,19 @@ Varyings DefaultPassVertex (uint vertexID : SV_VertexID) {
         vertexID <= 1 ? 0.0 : 2.0,
         vertexID == 1 ? 2.0 : 0.0
     );
+    if(_ProjectionParams.x < 0.0)
+    {
+        output.screenUV.y = 1.0 - output.screenUV.y;
+    }
     return output;
 }
 
+float4 GetSource(float2 screenUV) {
+    return SAMPLE_TEXTURE2D(_PostFXSource, sampler_linear_clamp, screenUV);
+}
+
 float4 CopyPassFragment (Varyings input) : SV_TARGET {
-    return float4(input.screenUV, 0.0, 1.0);
+    return GetSource(input.screenUV);
 }
 
 
