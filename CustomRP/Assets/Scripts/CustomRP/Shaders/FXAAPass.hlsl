a6b4002bca3dde79dc860e38e7a5f530ba0856b5
junbai <jun_bai@droidhang.com>
2024-12-03 17:54:47 +0800

    Blend factor

--- CustomRP/Assets/Scripts/CustomRP/Shaders/FXAAPass.hlsl
+++ CustomRP/Assets/Scripts/CustomRP/Shaders/FXAAPass.hlsl
@@ -4,37 +4,53 @@
 float4 _FXAAConfig;
 
 
-struct LumaNeighborhood {
-    float m, n, e, s, w;
+struct LumaNeighborhood
+{
+    float m, n, e, s, w, ne, se, sw, nw;
     float highest, lowest, range;
 };
 
-bool CanSkipFXAA (LumaNeighborhood luma)
+bool CanSkipFXAA(LumaNeighborhood luma)
 {
     return luma.range < max(_FXAAConfig.x, _FXAAConfig.y * luma.highest);
 }
 
+float GetSubpixelBlendFactor(LumaNeighborhood luma)
+{
+    float filter = 2.0 * (luma.n + luma.e + luma.s + luma.w);
+    filter += luma.ne + luma.nw + luma.se + luma.sw;
+    filter *= 1.0 / 12.0;
+    filter = abs(filter - luma.m);
+    filter = saturate(filter / luma.range);
+    filter = smoothstep(0, 1, filter);
+    return filter * filter;
+}
 
-float GetLuma (float2 uv, float uOffset = 0.0 ,float vOffset = 0.0)
+float GetLuma(float2 uv, float uOffset = 0.0, float vOffset = 0.0)
 {
     // return Luminance(GetSource(uv)); // 获取亮度
     // return sqrt(Luminance(GetSource(uv))); //人眼对暗部变化更敏锐
     // return GetSource(uv).g; //人眼对绿色更敏锐
-    uv += float2(uOffset,vOffset) * GetSourceTexelSize().xy;
+    uv += float2(uOffset, vOffset) * GetSourceTexelSize().xy;
     #if defined(FXAA_ALPHA_CONTAINS_LUMA)
-        return GetSource(uv).a;
+    return GetSource(uv).a;
     #else
         return GetSource(uv).g;
-    #endif  
+    #endif
 }
 
-LumaNeighborhood GetLumaNeighborhood (float2 uv) {
+LumaNeighborhood GetLumaNeighborhood(float2 uv)
+{
     LumaNeighborhood luma;
     luma.m = GetLuma(uv);
     luma.n = GetLuma(uv, 0.0, 1.0);
     luma.e = GetLuma(uv, 1.0, 0.0);
     luma.s = GetLuma(uv, 0.0, -1.0);
     luma.w = GetLuma(uv, -1.0, 0.0);
+    luma.ne = GetLuma(uv, 1.0, 1.0);
+    luma.se = GetLuma(uv, 1.0, -1.0);
+    luma.sw = GetLuma(uv, -1.0, -1.0);
+    luma.nw = GetLuma(uv, -1.0, 1.0);
     luma.highest = max(max(max(max(luma.m, luma.n), luma.e), luma.s), luma.w);
     luma.lowest = min(min(min(min(luma.m, luma.n), luma.e), luma.s), luma.w);
     luma.range = luma.highest - luma.lowest;
@@ -48,7 +64,7 @@ float4 FXAAPassFragment(Varyings input) : SV_TARGET
     {
         return 0.0;
     }
-    return luma.range;
+    return GetSubpixelBlendFactor(luma);
 }
 
 #endif
