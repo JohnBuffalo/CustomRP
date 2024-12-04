9d90a1108b4ff69da4c63aca11a651cc9489bf6a
junbai <jun_bai@droidhang.com>
2024-12-04 10:01:22 +0800

    Final blend

--- CustomRP/Assets/Scripts/CustomRP/Shaders/FXAAPass.hlsl
+++ CustomRP/Assets/Scripts/CustomRP/Shaders/FXAAPass.hlsl
@@ -104,12 +104,22 @@ float4 FXAAPassFragment(Varyings input) : SV_TARGET
     LumaNeighborhood luma = GetLumaNeighborhood(input.screenUV);
     if (CanSkipFXAA(luma))
     {
-        return 0.0;
+        return GetSource(input.screenUV);
     }
 
     FXAAEdge edge = GetFXAAEdge(luma);
-
-    return edge.pixelStep > 0.0 ? float4(1.0, 0.0, 0.0, 0.0) : 1.0;
+    float blendFactor = GetSubpixelBlendFactor(luma);
+    float2 blendUV = input.screenUV;
+    if(edge.isHorizontal)
+    {
+        blendUV.y += blendFactor * edge.pixelStep;
+    }else
+    {
+        blendUV.x += blendFactor * edge.pixelStep;
+    }
+    return GetSource(blendUV);
+    
+    // return edge.pixelStep > 0.0 ? float4(1.0, 0.0, 0.0, 0.0) : 1.0;
 }
 
 #endif
